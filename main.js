1c9ae47f0fa34b9ab0453fc69a60f6069db8c102
malak-samaan <121319191+malak-samaan@users.noreply.github.com>
2025-06-26 03:44:10 +0300

    Add main.js 1750898649900

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  // Load React app in development mode
+  win.loadURL('http://localhost:3000');
+
+  // Uncomment below to load local build files in production
+  // win.loadFile('build/index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
